690cec52a811939b76cc73641e64440def0af190
ryanLe <lbinh1910@gmail.com>
2026-01-18 22:59:54 -0500

    update

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 export default function Login() {
     return (
-    <main className= "flex flex-col items-center justify-center flex-1 w-full px-25">
+    <main className= "flex flex-col items-center justify-center flex-1 w-full px-25 mt-50">
         <div className="bg-white rounded-sm shadow-2xl flex w-2/3 max-w-4xl text-black">
             <div className="w-3/5 p-5" > 
-                <p> Log In </p> //sign in
+                <p className="text-center text-2xl font-extrabold"> Log In </p> 
              </div>
              <div className="w-2/5 bg-teal-100 rounded-tr-2xl rounded-br-2xl py-36 px-12">
                 <p className="text-2xl mb-5"> New To Our Wesbsite? </p>

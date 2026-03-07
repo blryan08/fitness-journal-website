a7a61d10878b0f0311ca5f7d040c26e4bbda8392
ryanLe <lbinh1910@gmail.com>
2026-03-07 12:03:55 -0500

    add background (looks okayish)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+
+import BubbleBackgroundDemo from "@/components/ui/bubble"
+
 export default function Home() {
   return (
-<div className="flex flex-wrap content-around ml-30 mt-20 flex-row">
+<div className="content-around ml-30 mt-20 relative ">
+  <div className="absolute inset-0 z-[-1]">
+  <BubbleBackgroundDemo/>
+  </div>
   <h1 className="text-5xl mr-25 font-serif bg-linear-to-r from-teal-500 via-blue-500 to-pink-500 bg-clip-text text-transparent"> TRAIN . TRACK . TRANSFORM </h1>
   <h2 className = "text-5xl max-w-md ml-10  "> Your Desitnation to Building a Better Fitness Journey</h2>
-  <div className = "flex w-120 h-120 bg-white rounded-2xl">
-    <img>
-    </img>
-
   </div>
-</div>
+
+
 );
 }
